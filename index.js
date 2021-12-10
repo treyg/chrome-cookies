ff4f9dad8bd7e3177e8639a17ebe795a6e0e70e7
Trey Gordon <41927921+treyg@users.noreply.github.com>
2021-12-10 11:32:47 -0500

    Update extension to run on icon click

--- index.js
+++
@@ -1,5 +0,0 @@
-//delete secure cookie
-function removeSecureCookie(name) {
-  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT; domain=staging-na-wwwinc.demandware.net; secure`;
-}
-removeSecureCookie("dwsid");
